5cff67b75681015c96a525dfedbbe7fe32d4aabb
Geoill <bobo@geoill.com>
2013-08-03 11:32:56 -0400

    Add a title help to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#puts the title defualt base title on the page
+	def title	
+		base_title = "Ruby on Rails Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
